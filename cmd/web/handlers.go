e6ae2c60ad16c8d85732d6a32b30d5d5ab6e3c0a
Junior Paula <luke.junnior@icloud.com>
2023-09-13 16:54:31 -0300

    Getting payment intent - get a recipient for the transaction and show onverview payment succeeded page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,3 +8,32 @@ func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 }
+
+func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.errorLog.Println(err.Error())
+		return
+	}
+
+	cardHolder := r.Form.Get("cardholder_name")
+	email := r.Form.Get("cardholder_email")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	paymentAmount := r.Form.Get("payment_amount")
+	paymentCurrency := r.Form.Get("payment_currency")
+
+	data := make(map[string]interface{})
+	data["cardholder"] = cardHolder
+	data["email"] = email
+	data["paymentIntent"] = paymentIntent
+	data["paymentMethod"] = paymentMethod
+	data["paymentAmount"] = paymentAmount
+	data["paymentCurrency"] = paymentCurrency
+
+	if err := app.renderTemplate(w, r, "payment-succeeded", &templateData{Data: data}); err != nil {
+		app.errorLog.Println(err.Error())
+		return
+	}
+
+}
