c68abb0c568fbe8cb197c7732fec115b2731bff4
Junior Paula <luke.junnior@icloud.com>
2023-09-16 10:28:16 -0300

    Create method to insert a new transaction into database

--- internal/models/models.go
+++ internal/models/models.go
@@ -125,3 +125,38 @@ func (m *DBModel) GetWidget(ID int) (Widget, error) {
 
 	return widget, nil
 }
+
+// InsertOrder inserts an net txn into the database and return its id.
+func (m *DBModel) InsertTransacetion(txn Transaction) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `
+		insert into transactions
+			(amount, currency, last_for, bank_return_code, transaction_status_id, created_at, updated_at)
+		values	
+			(?, ?, ?, ?, ?, ?, ?)
+	`
+
+	result, err := m.DB.ExecContext(ctx, stmt,
+		txn.Amount,
+		txn.Currency,
+		txn.Lastfor,
+		txn.BankReturnCode,
+		txn.TransactionStatusID,
+		time.Now(),
+		time.Now(),
+	)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	return int(id), nil
+}
