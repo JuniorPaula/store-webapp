89f55f7147379a71d8a471c326c18ad2373a5094
Junior Paula <luke.junnior@icloud.com>
2023-09-19 07:37:35 -0300

    Create a helper func to write json

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,6 +7,28 @@ import (
 	"net/http"
 )
 
+// writeJSON is a helper which encodes the given data to JSON, writes it with
+// a 200 OK status code, and sets the Content-Type header to application/json.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		app.errorLog.Println(err)
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(out)
+
+	return nil
+}
+
 // readJSON is a helper which decodes the JSON request body into the interface
 // provided as a parameter.
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
