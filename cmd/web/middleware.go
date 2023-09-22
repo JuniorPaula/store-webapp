b58cec051b345b54863c7aa3c01a8322310cef00
Junior Paula <luke.junnior@icloud.com>
2023-09-22 15:49:10 -0300

    Create middleware on the client to check authentication

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -5,3 +5,13 @@ import "net/http"
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
 }
+
+func (app *application) Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// If the user is not authenticated, redirect them to the login page and return from the middleware chain so that no subsequent handlers in the chain are executed.
+		if !app.Session.Exists(r.Context(), "user_id") {
+			http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+		}
+		next.ServeHTTP(w, r)
+	})
+}
